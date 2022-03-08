37a7c9d29ced932481d198e168fb2b6eb9280a24
gorilla-kim <00ghks22@naver.com>
2022-03-09 00:12:18 +0900

    feat: 뱃지 클릭을 통해 newTab.html 파일이 토글이 되도록 개발

--- background.js
+++ background.js
@@ -1,12 +1,27 @@
-function openNewTap() {
-    chrome.tabs.create({'url':'chrome://newtab'})
+let windowId = 0
+
+function openNewWindow(windowId) {
+    chrome.storage.sync.get('isOpen', async function(result) {
+        if(result.isOpen) {
+            chrome.storage.sync.remove('isOpen');
+            chrome.windows.remove(windowId)
+        } else {
+            chrome.storage.sync.set({isOpen: true});
+            chrome.windows.create({
+                url: chrome.runtime.getURL("/newTab/index.html"),
+                type: "popup",
+                height: 800,
+                width: 500
+            }, function(window){
+                windowId = window.id
+            })
+        }
+    });
 }
-function openNewWindow() {
-    chrome.windows.create({
-        url: chrome.runtime.getURL("newTab.html"),
-        type: "popup",
-        height: 800,
-        width: 500
-    })
-}
-chrome.action.onClicked.addListener(openNewWindow)
+
+chrome.action.onClicked.addListener(()=>openNewWindow(windowId))
+chrome.windows.onRemoved.addListener(function(targetWindowId) {
+    if(targetWindowId === windowId) {
+        chrome.storage.sync.remove('isOpen');
+    }
+})
